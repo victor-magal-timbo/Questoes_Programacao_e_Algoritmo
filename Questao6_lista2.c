ab9d46b21844c240e2dc24a4f8288e1a6a88575c
Victor Manoel Magalhães Timbó <68547537+victor-magal-timbo@users.noreply.github.com>
2023-09-24 17:46:54 -0300

    Create Questao6_lista2.c

---
+++ Questao6_lista2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+6. Roberto deseja adicionar mais uma funcionalidade ao seu programa. Ele possui uma lista de 10 IDs
+de filiais. O algoritmo deve fazer o seguinte:
+(a) Solicitar ao usu ́ario que insira um ID espec ́ıfico;
+(b) Verificar se o ID inserido est ́a presente na lista de IDs das filiais;
+(c) Se o ID estiver na lista, o programa deve informar que o ID pertence a uma filial da lista;
+(d) Caso contr ́ario, o programa deve informar que o ID digitado n ̃ao corresponde a nenhuma filial da lista.
+
