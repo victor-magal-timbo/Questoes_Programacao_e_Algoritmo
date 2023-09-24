fb7aedd4a6339adb61199b738b9897f5f627de47
Victor Manoel Magalhães Timbó <68547537+victor-magal-timbo@users.noreply.github.com>
2023-09-24 17:52:19 -0300

    Create Questao7_lista2.c

---
+++ Questao7_lista2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+7. Por fim, Roberto deseja que o programa, ao ler o faturamento de 8 filiais, realize as seguintes tarefas:
+(a) Armazene os faturamentos em um  ́unico vetor;
+(b) Calcule a soma de todos os elementos desse vetor;
+(c) Conte quantos deles representam lucro (valores positivos).
+
+float fl[8];
+    float soma;
+    int i = 0, cont = 0;
+    for(i=0;i<8;i++)
+    {
+        printf("Digite o valor da filial %d: ", i+1);
+        scanf("%f", &fl[i]);
+        if(fl[i] > 0)
+        {
+            cont++;
+        }
+        soma += fl[i];
+        system("cls");
+    }
+    printf("\n%d filiais estao dando lucro.\n%.2f\n", cont, soma);
