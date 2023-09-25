ae5cf03eb17fdc7f75be12dd5ecb5db35e2630b6
Victor Manoel Magalhães Timbó <68547537+victor-magal-timbo@users.noreply.github.com>
2023-09-25 08:57:29 -0300

    Update Questao6_lista2.c

--- Questao6_lista2.c
+++ Questao6_lista2.c
@@ -8,3 +8,20 @@ de filiais. O algoritmo deve fazer o seguinte:
 (c) Se o ID estiver na lista, o programa deve informar que o ID pertence a uma filial da lista;
 (d) Caso contr ́ario, o programa deve informar que o ID digitado n ̃ao corresponde a nenhuma filial da lista.
 
+void questoesPA(){
+  int id[] = {1,2,3,4,5,6,7,8,9,10};
+    int ent, i, id_v = 0;
+    printf("Digite o ID da filial: ");
+    scanf("%d", &ent);
+    for(i=0;i<10;i++){
+        if(ent == id[i]){
+            ent = id[i];
+            id_v = ent;
+            printf("FILIAL: %d\n", ent);
+            break;
+        }
+    }
+    if(id_v == 0){
+        printf("ID nao corresponde as filiais da lista\n");
+    }
+}
